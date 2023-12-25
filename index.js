45764f644f09048575edbc786bff62d66365db73
Acer <codecinnpro@gmail.com>
2023-12-25 11:33:58 +0800

    2

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000;
 const mongoose = require('mongoose');
+const swaggerUi = require("swagger-ui-express");
+const swaggerJSDoc = require('swagger-jsdoc');
 
 app.use(express.json())
 
@@ -16,6 +18,140 @@ db.once("open",()=>{
 app.get('/', (req, res) => {
    res.send('Hello World!')
 })
+
+const options = {
+    definition:{
+        openapi: "3.0.3",
+        info:{
+            title: "Visitor Management System BERR G6",
+            version: "0.1",
+            description:"Visitor Management System with admin, host, visitors. A system to issue visitors pass and store the record into the cloud database, Mongodb Atlas.",
+            contact:{
+                name: "Hee Yee Cinn",
+                url:"cinn.com",
+                email:"b022110115@student.utem.edu.my"
+            },
+
+        },
+        tags:[
+            {name:'Login', description:"Default endpoints"},
+            {name: 'Admin', description:"Admin operation"},
+            {name: 'Host', description:"Host operation"},
+        ],
+        components:{
+            securitySchemes:{
+                Authorization:{
+                    type: "http",
+                    scheme: "bearer",
+                    bearerFormat: "JWT",
+                    value: "Bearer <JWT token here>",
+                    description: "This is for authentication, you must logout to change the JWT token"
+                }
+            }
+        },
+        servers: [
+            {
+                url:"http://localhost:3000/"
+                // url:"https://swaggerg6.azurewebsites.net/"
+            }
+        ],
+    },
+    //all the route.js file store inside the route file 
+    apis:["./index.js"],
+};
+
+/**
+* @swagger
+* components:
+*   schemas:
+*     Visit:
+*       type: object
+*       required:
+*         - purposeOfVisit
+*         - phoneNumber
+*       properties:
+*         purposeOfVisit:
+*           type: string
+*           description: The purpose of the visit
+*         phoneNumber:
+*           type: number
+*           description: The phone number of the visitor
+*         visitTime:
+*           type: string
+*           format: date-time
+*           description: The time of the visit
+*       example:
+*         purposeOfVisit: Meeting
+*         phoneNumber: 1234567890
+*
+*     Visitor:
+*       type: object
+*       required:
+*         - name
+*       properties:
+*         name:
+*           type: string
+*           description: The name of the visitor
+*         visits:
+*           type: array
+*           items:
+*             $ref: '#/components/schemas/Visit'
+*       example:
+*         name: John Doe
+*         visits:
+*           - purposeOfVisit: Meeting
+*             phoneNumber: 1234567890
+*             visitTime: '2023-01-01T12:00:00Z'
+*
+*     User:
+*       type: object
+*       required:
+*         - username
+*         - password
+*         - email
+*         - phoneNumber
+*         - category
+*       properties:
+*         username:
+*           type: string
+*           description: The username of the user
+*         password:
+*           type: string
+*           description: The password of the user
+*         email:
+*           type: string
+*           format: email
+*           description: The email address of the user
+*         phoneNumber:
+*           type: number
+*           description: The phone number of the user
+*         category:
+*           type: string
+*           enum:
+*             - host
+*             - admin
+*           description: The category of the user (host or admin)
+*         visitors:
+*           type: array
+*           items:
+*             $ref: '#/components/schemas/Visitor'
+*       example:
+*         username: user123
+*         password: password123
+*         email: user@exa\mple.com
+*         phoneNumber: 1234567890
+*         category: host
+*         visitors:
+*           - name: John Doe
+*             visits:
+*               - purposeOfVisit: Meeting
+*                 phoneNumber: 1234567890
+*                 visitTime: '2023-01-01T12:00:00Z'
+*/
+
+const spacs = swaggerJSDoc(options);
+app.use("/g6", swaggerUi.serve, swaggerUi.setup(spacs));
+
 
 app.listen(port, () => {
    console.log(`Example app listening on port ${port}`)
