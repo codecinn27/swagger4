00e5a28e022f28f3652a2e25a0bdf061796a72ab
Acer <codecinnpro@gmail.com>
2023-12-25 16:02:41 +0800

    17

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const swaggerUi = require("swagger-ui-express");
 const swaggerJSDoc = require('swagger-jsdoc');
 //const loginRouter = require('./routes/login');
 const adminRouter = require('./routes/admin');
+const User = require('./model/user');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+
 app.use(express.json())
 
 mongoose.connect('mongodb+srv://codecinnpro:9qLtJIAG9k8G1Pe8@cluster0.egrjwh1.mongodb.net/vms_2?retryWrites=true&w=majority')
@@ -22,6 +26,62 @@ app.get('/', (req, res) => {
 
 //app.use('/login', loginRouter);
 app.use('/admin', adminRouter);
+
+// POST route for user login
+app.post('/login',async(req, res) =>{
+    try {
+      // Implement your login logic (e.g., validate credentials against the database)
+      const { username, password } = req.body;
+      const user = await User.findOne({username});
+  
+      if (!user) {
+        return res.status(401).json({ error: 'Invalid credentials' });
+      }
+  
+      // Compare the provided password with the hashed password using bcrypt
+      const isPasswordValid = await bcrypt.compare(password, user.password);
+  
+      if (!isPasswordValid) {
+          return res.status(401).json({ error: 'Invalid credentials' });
+      }
+  
+      // Generate a JWT token
+      const token = jwt.sign({ userId: user._id, category: user.category }, 'vms2', {
+        expiresIn: '1h',
+      });
+  
+      // Check the user's category and generate the appropriate link
+      let redirectLink;
+      if (user.category === 'host') {
+          redirectLink = `/host/${user._id}`;
+      } else if (user.category === 'admin') {
+          redirectLink = `/admin`;
+      }
+  
+  
+      console.log("JWT:",token);
+      res.json({
+          token,
+          category: user.category,
+          redirectLink,
+          "GET": `/g6/${redirectLink}`,
+          Authorization: token,
+          "Content-Type": "application/json",
+        });
+        
+        
+    } catch (error) {
+      console.error('Error during login:', error);
+        // Log additional information about the error
+      console.error('Error Stack:', error.stack);
+      // Handle different types of errors
+      if (error.name === 'ValidationError') {
+        return res.status(400).json({ error: 'Invalid input data' });
+      } else {
+        return res.status(500).json({ error: 'Internal Server Error' });
+      }
+    }
+  });
 
 const options = {
     definition:{
@@ -55,8 +115,8 @@ const options = {
         },
         servers: [
             {
-                //url:"http://localhost:3000/",
-                url:"https://swaggerg6.azurewebsites.net/"
+                url:"http://localhost:3000/",
+                //url:"https://swaggerg6.azurewebsites.net/"
             }
         ],
     },
---
+++ model/user.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const Visitor = require('./visitor');
+
+const userSchema = new mongoose.Schema({
+    username:{
+        type: String,
+        required: true
+    },
+    password:{
+        type: String,
+        required: true,
+        min: 5
+    },
+    email:{
+        type: String,
+        required: true
+    },
+    phoneNumber:{
+        type: Number,
+        required: true
+    },
+    category:{
+        type: String,
+        enum: ['host','admin']
+    },
+    visitors: [Visitor.schema]  // Embed an array of visitors within each host
+});
+
+// Define a pre-save hook to hash the password before saving to the database
+userSchema.pre('save', async function (next) {
+    try {
+        if (this.isModified('password') || this.isNew) {
+            const hashedPassword = await bcrypt.hash(this.password, 10);
+            this.password = hashedPassword;
+        }
+        next();
+    } catch (error) {
+        next(error);
+    }
+});
+
+module.exports = mongoose.model('User', userSchema);
