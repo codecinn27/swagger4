d1fcf17b491696b3694c76bedba3ebcc4d43ecac
Acer <codecinnpro@gmail.com>
2023-12-25 21:07:32 +0800

    26

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const mongoose = require('mongoose');
 const swaggerUi = require("swagger-ui-express");
 const swaggerJSDoc = require('swagger-jsdoc');
 const Visit = require('./model/visit');
-
+const User = require('./model/user');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 app.use(express.json())
 
 mongoose.connect('mongodb+srv://codecinnpro:9qLtJIAG9k8G1Pe8@cluster0.egrjwh1.mongodb.net/vms_2?retryWrites=true&w=majority')
@@ -70,6 +72,57 @@ app.get('/admin/visits',async(req,res)=>{
     }  
 });
 
+app.post('/login',async(req, res) =>{
+    try {
+      // Implement your login logic (e.g., validate credentials against the database)
+      const { username, password } = req.body;
+      const user = await User.findOne({username});
+  
+      if (!user) {
+        return res.status(401).json({ error: 'Invalid credentials' });
+      }
+  
+      // Compare the provided password with the hashed password using bcrypt
+      const isPasswordValid = await bcrypt.compare(password, user.password);
+  
+      if (!isPasswordValid) {
+          return res.status(401).json({ error: 'Invalid credentials' });
+      }
+  
+      // Generate a JWT token
+      const token = jwt.sign({ userId: user._id, category: user.category }, 'vms2', {
+        expiresIn: '1h',
+      });
+  
+      // Check the user's category and generate the appropriate link
+      let redirectLink;
+      if (user.category === 'host') {
+          redirectLink = `/host/${user._id}`;
+      } else if (user.category === 'admin') {
+          redirectLink = `/admin`;
+      }
+      console.log("JWT:",token);
+      res.json({
+          token,
+          category: user.category,
+          redirectLink,
+          Authorization: token,
+          "Content-Type": "application/json",
+        });
+        
+        
+    } catch (error) {
+      console.error('Error during login:', error);
+        // Log additional information about the error
+      console.error('Error Stack:', error.stack);
+      // Handle different types of errors
+      if (error.name === 'ValidationError') {
+        return res.status(400).json({ error: 'Invalid input data' });
+      } else {
+        return res.status(500).json({ error: 'Internal Server Error' });
+      }
+    }
+});
 
 /**
 * @swagger
