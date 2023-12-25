343ff46ec9e709ab7b207656576737789c63ef66
Acer <codecinnpro@gmail.com>
2023-12-25 22:30:19 +0800

    main

--- index.js
+++ index.js
@@ -142,57 +142,6 @@ app.get('/', (req, res) => {
 *      
 */
 
-app.post('/login',async(req, res) =>{
-    try {
-      // Implement your login logic (e.g., validate credentials against the database)
-      const { username, password } = req.body;
-      const user = await User.findOne({username});
-  
-      if (!user) {
-        return res.status(401).json({ error: 'Invalid credentials' });
-      }
-  
-      // Compare the provided password with the hashed password using bcrypt
-      const isPasswordValid = await bcrypt.compare(password, user.password);
-  
-      if (!isPasswordValid) {
-          return res.status(401).json({ error: 'Invalid credentials' });
-      }
-  
-      // Generate a JWT token
-      const token = jwt.sign({ userId: user._id, category: user.category }, 'vms2', {
-        expiresIn: '1h',
-      });
-  
-      // Check the user's category and generate the appropriate link
-      let redirectLink;
-      if (user.category === 'host') {
-          redirectLink = `/host/${user._id}`;
-      } else if (user.category === 'admin') {
-          redirectLink = `/admin`;
-      }
-      res.json({
-          username,
-          token,
-          category: user.category,
-          redirectLink,
-          Authorization: token,
-          "Content-Type": "application/json",
-        });
-        
-        
-    } catch (error) {
-      console.error('Error during login:', error);
-        // Log additional information about the error
-      console.error('Error Stack:', error.stack);
-      // Handle different types of errors
-      if (error.name === 'ValidationError') {
-        return res.status(400).json({ error: 'Invalid input data' });
-      } else {
-        return res.status(500).json({ error: 'Internal Server Error' });
-      }
-    }
-});
 
 /**
 * @swagger
